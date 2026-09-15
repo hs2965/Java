2357ecf9834e14afd8cf1d42577413043233b3e1
hs2965 <hs2965@srmist.edu.in>
2026-09-15 22:30:11 +0530

    week1

---
+++ week1/practise/qL1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class qL1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        boolean isPrime = true;
+
+        if (number < 2) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Is the number " + number + " a Prime number? " + isPrime);
+    }
+}
---
+++ week1/practise/qL2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class qL2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+
+        System.out.println("The right-angled triangle pattern for " + rows + " rows is");
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
